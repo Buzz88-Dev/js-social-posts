754d3b0cf1a9f92dc9aa79d3df46110693d0a7d5
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-20 18:00:04 +0200

    aggiornamento

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const elements = [
+const arrayElements = [
     {
         "id": 1,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
@@ -56,56 +56,146 @@ const elements = [
     }
 ];
 
-// creo i miei collegamenti e vari elementi
+let myArrayId = arrayElements.map((item) => {
+    let element = item.id;
+    return element;
+})
+console.log(myArrayId);
+
+let myArrayContent = arrayElements.map((item) => {
+    let element = item.content;
+    return element;
+})
+console.log(myArrayContent);
+
+let myArrayMedia = arrayElements.map((item) => {
+    let element = item.media;
+    return element;
+})
+console.log(myArrayMedia);
+
+// myArrayAuthor
+
+let myArrayLikes = arrayElements.map((item) => {
+    let element = item.likes;
+    return element;
+})
+console.log(myArrayLikes);
+
+let myArrayCreated = arrayElements.map((item) => {
+    let element = item.created;
+    return element;
+})
+console.log(myArrayCreated);
+
+
+
+
+
+
+
+
+
+
+
 let divContainer = document.getElementById("container");
-divContainer.classList.add("post-list");
+divClassPost();
 
-// creo il div con classe post che si appende al div container
-let post = document.createElement("div");
-post.classList.add("post");
-divContainer.append(post);
 
-// creo il primo blocco di div interni al div con classe post
-let post_header = document.createElement("div");
-post_header.classList.add("post_header");
-post.append(post_header);
+function divClassPost(){
+    // creo i miei collegamenti e vari elementi
+    let divContainer = document.getElementById("container");
+    divContainer.classList.add("post-list");
 
-let post_meta = document.createElement("div");
-post_meta.classList.add("post-meta");
-post_header.append(post_meta);
+    // creo il div con classe post che si appende al div container
+    let post = document.createElement("div");
+    post.classList.add("post");
+    divContainer.append(post);
 
-let post_meta_icon = document.createElement("div");
-post_meta_icon.classList.add("post-meta-icon");
-post_meta.append(post_meta_icon);
+    // creo il primo blocco di div interni al div con classe post
+    let post_header = document.createElement("div");
+    post_header.classList.add("post_header");
+    post.append(post_header);
 
-let img_profile_pic = document.createElement("img");
-img_profile_pic.classList.add("profile-pic");
-img_profile_pic.src;
-post_meta_icon.append(img_profile_pic);
+    let post_meta = document.createElement("div");
+    post_meta.classList.add("post-meta");
+    post_header.append(post_meta);
 
-let post_meta_data = document.createElement("div");
-post_meta_data.classList.add("post-meta__data");
-post_meta.append(post_meta_data);
+    let post_meta_icon = document.createElement("div");
+    post_meta_icon.classList.add("post-meta-icon");
+    post_meta.append(post_meta_icon);
 
-let post_meta_data_author = document.createElement("div");
-post_meta_data_author.classList.add("post-meta__author");
-post_meta_data.append(post_meta_data_author);
+    let img_profile_pic = document.createElement("img");
+    img_profile_pic.classList.add("profile-pic");
+    img_profile_pic.src;
+    img_profile_pic.alt;
+    post_meta_icon.append(img_profile_pic);
 
-let post_meta_time = document.createElement("div");
-post_meta_time.classList.add("post-meta__time");
-post_meta_data.append(post_meta_time)
+    let post_meta_data = document.createElement("div");
+    post_meta_data.classList.add("post-meta__data");
+    post_meta.append(post_meta_data);
 
-// creo il secondo blocco di div interni al div con classe post
-let post_text = document.createElement("div");
-post_text.classList.add("post__text");
-post.append(post_text);
+    let post_meta_data_author = document.createElement("div");
+    post_meta_data_author.classList.add("post-meta__author");
+    post_meta_data.append(post_meta_data_author);
 
-let post_image = document.createElement("div");
-post_image.classList.add("post__image");
-post.append(post_image);
+    let post_meta_time = document.createElement("div");
+    post_meta_time.classList.add("post-meta__time");
+    post_meta_data.append(post_meta_time)
 
-let img = document.createElement("img");
-img.src;
-post_image.append(img);
+    // creo il secondo blocco di div interni al div con classe post
+    let post_text = document.createElement("div");
+    post_text.classList.add("post__text");
+    post.append(post_text);
 
-// creo il terzo blocco di div interni al div con classe post
+    let post_image = document.createElement("div");
+    post_image.classList.add("post__image");
+    post.append(post_image);
+
+    let img = document.createElement("img");
+    img.src;
+    post_image.append(img);
+
+    // creo il terzo blocco di div interni al div con classe post
+    let post_footer = document.createElement("div");
+    post_footer.classList.add("post__footer");
+    post.append(post_footer);
+
+    let likes_js_likes = document.createElement("div");
+    likes_js_likes.classList.add("likes");
+    likes_js_likes.classList.add("js-likes");
+    post_footer.append(likes_js_likes);
+
+    let likes_cta = document.createElement("div");
+    likes_cta.classList.add("likes__cta");
+    likes_js_likes.append(likes_cta);
+
+    let like_button = document.createElement("a");
+    like_button.classList.add("like-button");
+    like_button.classList.add("js-like-button");
+    // manca data-postid="1"
+    likes_cta.append(like_button);
+
+    let like_button_icon = document.createElement("i");
+    like_button_icon.classList.add("like-button__icon");
+    like_button_icon.classList.add("fas");
+    like_button_icon.classList.add("fa-thumbs-up");
+    // aria-hidden="true"
+    like_button.append(like_button_icon);
+
+    let like_button_label = document.createElement("span");
+    like_button_label.classList.add("like-button__label");
+    like_button_label.innerHTML = " Mi Piace";
+    like_button.append(like_button_label);
+
+    let likes_counter = document.createElement("div");
+    likes_counter.classList.add("likes__counter");
+    likes_counter.innerHTML = "Piace a " + " persone";
+    likes_js_likes.append(likes_counter);
+
+    let js_likes_counter = document.createElement("b");
+    js_likes_counter.classList.add("js-likes-counter");
+    // manca id="like-counter-1"
+    js_likes_counter.innerHTML = "80";
+    likes_counter.append(js_likes_counter);
+}
